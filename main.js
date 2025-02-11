f8ef4829087a2f7f5c35b8280a682abe143be10e
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-11 16:14:39 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/* 
+programma che stampa i numeri da 1 a 100
+
+SE il numero è divisibile per 3 stampa Fizz
+SE il numero è divisibile per 5 stampa Buzz
+
+SE il numero è divisibile per 3 e 5 stampa FizzBuzz
+*/
+
+for (let i = 1; i < 100; i++) {             // definisco la variabile i, se è inferiore a 100 aumenta i di 1
+  if (i % 3 == 0 && i % 5 == 0) {           // se i è divisibile senza resto per 3 e 5
+    console.log("FizzBuzz");                // stampo fizzbuzz
+  }
+
+}
