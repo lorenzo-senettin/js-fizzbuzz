6b57b0fce919f1b32c3c548fd8858d75f8585069
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-11 16:16:43 +0100

    add number divisible only by 3

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ SE il numero è divisibile per 3 e 5 stampa FizzBuzz
 for (let i = 1; i < 100; i++) {             // definisco la variabile i, se è inferiore a 100 aumenta i di 1
   if (i % 3 == 0 && i % 5 == 0) {           // se i è divisibile senza resto per 3 e 5
     console.log("FizzBuzz");                // stampo fizzbuzz
+  } else if (i % 3 == 0) {                  // se i è divisibile senza resto per 3
+    console.log("Fizz");                    // stampo fizz
   }
 
 }
