5580f486094cbc597cc8338146e574925c6552e7
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-11 16:20:15 +0100

    add number numbers not divisible by 3 or 5

--- main.js
+++ main.js
@@ -7,14 +7,15 @@ SE il numero è divisibile per 5 stampa Buzz
 SE il numero è divisibile per 3 e 5 stampa FizzBuzz
 */
 
-for (let i = 1; i < 100; i++) {             // definisco la variabile i, se è inferiore a 100 aumenta i di 1
+for (let i = 1; i < 101; i++) {             // definisco la variabile i, se è inferiore a 100 aumenta i di 1
   if (i % 3 == 0 && i % 5 == 0) {           // se i è divisibile senza resto per 3 e 5
     console.log("FizzBuzz");                // stampo fizzbuzz
   } else if (i % 3 == 0) {                  // se i è divisibile senza resto per 3
     console.log("Fizz");                    // stampo fizz
   } else if (i % 5 == 0) {                  // se i è divisibile senza resto per 5
     console.log("Buzz");                    // stampo buzz
-
+  } else {                                  // se il numero non rientra nelle altre opzioni
+    console.log(i);                         // stampa il numero
   }
 
 }
